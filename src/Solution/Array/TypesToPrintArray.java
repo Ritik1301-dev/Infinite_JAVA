1619688c7818fc648b3c6904afbea7a6dade92d7
Ritik1301-dev <koseritik67@gmail.com>
2026-07-04 16:39:04 +0530

    commit 4

---
+++ src/Solution/Array/TypesToPrintArray.java
@@ -0,0 +1,24 @@
+package Solution.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class TypesToPrintArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i< arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int temp : arr){
+            System.out.print(temp + " ");
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        System.out.print(Arrays.toString(arr));  // For this method we need to import the Arrays package.
+    }
+}
---
+++ src/Solution/PatternPrograms/HollowSquare.java
@@ -0,0 +1,8 @@
+//package Solution.PatternPrograms;
+//import java.util.Scanner;
+//public class HollowSquare {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc
+//    }
+//}
