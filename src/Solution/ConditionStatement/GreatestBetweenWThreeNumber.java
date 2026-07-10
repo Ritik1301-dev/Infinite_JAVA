c621277eda5ab48a335f8f35ad158a24cac9b357
Ritik1301-dev <koseritik67@gmail.com>
2026-07-10 23:59:33 +0530

    commit 13

--- src/Solution/ConditionStatement/GreatestBetweenWThreeNumber.java
+++ src/Solution/ConditionStatement/GreatestBetweenWThreeNumber.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class GreatestBetweenWThreeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Inter your  1st value : ");
         int a = sc.nextInt();
+        System.out.print("Inter your  2th value : ");
         int b = sc.nextInt();
+        System.out.print("Inter your  3rd value : ");
         int c = sc.nextInt();
+        System.out.print("Greatest Number is : ");
         if(a>b) if(a>c) System.out.println(a);
         else System.out.println(c);
         else if(b>c) System.out.println(b);
--- src/Solution/SearchingAlgorithm/LinearSearch.java
+++ src/Solution/SearchingAlgorithm/LinearSearch.java
@@ -1,14 +1,24 @@
 package Solution.SearchingAlgorithm;
+import java.util.Scanner;
 public class LinearSearch {
-       static int linearSearch(int []arr, int target){
-           for (int i=0;i<arr.length;i++){
-               if (arr[i]==target) return i;
+           static int linearSearch(int []arr, int target){
+               for (int i=0;i<arr.length;i++){
+                   if (arr[i]==target) return i;
+               }
+               return -1;
            }
-           return -1;
-       }
     public static void main(String[] args) {
-           int [] arr = {123,543,344,65,322,56,7,78888,8765};
-           int target = 788;
+           Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+           int [] arr = new int[size];
+           for (int i=0;i<size;i++){
+               System.out.print("Enter "+ (i+1) + "th element : ");
+               arr[i] = sc.nextInt();
+           }
+        System.out.print("Enter the target value : ");
+           int target = sc.nextInt();
            int index = linearSearch(arr,target);
            if(index == -1) System.out.println("Target Not Found");
            else System.out.println("Target Found at " + index +  "th index" );
