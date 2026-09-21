50aba45c22f66f04a78b6aa9550a018ed29bbdb4
Ritik1301-dev <koseritik67@gmail.com>
2026-09-21 22:17:13 +0530

    commit 36

---
+++ src/Solution/String/ArrayToString.java
@@ -0,0 +1,9 @@
+package Solution.String;
+
+public class ArrayToString {
+    public static void main(String[] args) {
+        char [] ch = {'a','b','c','d'};
+        String  s = new String(ch);
+        System.out.println(s);
+    }
+}
--- src/Solution/String/pratice.java
+++ src/Solution/String/pratice.java
@@ -6,26 +6,14 @@ import java.util.Arrays;
 
 public class pratice {
     public static void main(String[] args) {
-        String s = "Ten millon dollar";
-        System.out.println(s.length());
-        System.out.println(s.charAt(4));
-        System.out.println(s.toLowerCase());
-        System.out.println(s.toUpperCase());
-        System.out.println(s.startsWith("T"));
-        System.out.println(s.endsWith("llar"));
-        System.out.println(s.substring(2));
-        System.out.println(s.substring(3,6));
-        System.out.println(s.indexOf("milllon"));
-        System.out.println(s.contains("q"));
-        System.out.println(s.trim());
-         String str[] =  s.split(" ");
-         for(String temp : str){
-             System.out.println(temp);
-         }
-         char ch[] = s.toCharArray();
-        System.out.println(Arrays.toString(ch));
-        String s2 = "Ten millon Dollar";
-        System.out.println(s.equals(s2));
-        System.out.println(s.equalsIgnoreCase(s2));
+        String s1 = "Hello Bhai...";  // literal constant
+        String s2 = "Hello Bhai...";
+
+        String s3 = new String("Hello Bhai..."); // string object
+        String s4 = new String("Hello Bhai...");
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s3.equals(s4));
+
     }
 }
