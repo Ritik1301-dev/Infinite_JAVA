34f11bc04aab54e718814865837c2164598c5fce
Ritik1301-dev <koseritik67@gmail.com>
2026-09-22 16:24:01 +0530

    commit 40

---
+++ src/Solution/OneDimensionalArray/Clg_Question7.java
@@ -0,0 +1,15 @@
+package Solution.OneDimensionalArray;
+
+public class Clg_Question7 {
+    public static void main(String[] args) {
+        int [] arr  = {5,2,9,1,7,3};
+        int key = 9;
+        boolean found = false;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == key){
+                found = true;
+            }
+        }
+        System.out.println(found);
+    }
+}
