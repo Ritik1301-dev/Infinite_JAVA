49a077842381de4821d9ff3380498c13e1552031
Ritik1301-dev <koseritik67@gmail.com>
2026-09-22 19:27:30 +0530

    commit 45

---
+++ src/Solution/String/CountVowelAndConsonants.java
@@ -0,0 +1,22 @@
+package Solution.String;
+
+import java.util.Scanner;
+
+public class CountVowelAndConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Eneter a value : ");
+        String str = sc.nextLine();
+        int v=0 ,c=0, s=0;
+     for (int i=0;i<str.length();i++){
+         char ch = str.charAt(i);
+         switch (ch){
+             case 'a','e','i','o','u'->v++;
+             case ' '->s++;
+             default->c++;
+         }
+     }
+        System.out.println("Vowel : " + v);
+        System.out.println("Consonant : " + c);
+    }
+}
