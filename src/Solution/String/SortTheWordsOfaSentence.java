db6efdf2aa0bf0e8396dace234d6dc995e21ab25
Ritik1301-dev <koseritik67@gmail.com>
2026-09-24 20:35:16 +0530

    commit 51

---
+++ src/Solution/String/SortTheWordsOfaSentence.java
@@ -0,0 +1,23 @@
+package Solution.String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortTheWordsOfaSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String arr [] = str.split(" ");
+
+        for (int i=0;i<arr.length-1;i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0){
+                    String temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
