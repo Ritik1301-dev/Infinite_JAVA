52f0608ab9f66e9bc84b07fa9a9f09278dae679d
Ritik1301-dev <koseritik67@gmail.com>
2026-09-24 15:43:06 +0530

    commit 49

---
+++ src/Solution/OneDimensionalArray/Clg_Question9.java
@@ -0,0 +1,43 @@
+package Solution.OneDimensionalArray;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Clg_Question9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i=0;i<arr.length;i++){
+            System.out.println((i+1) + "th element");
+            arr[i] = sc.nextInt();
+        }
+        int evenNum = 0;
+        int oddNum = 0;
+        for(int i=-0;i<arr.length;i++){
+          if(arr[i] % 2==0){
+              evenNum++;
+          }else {
+              oddNum++;
+          }
+        }
+
+        int [] evenArr = new int[evenNum];
+        int [] oddArr = new int[oddNum];
+        int e=0;
+        int o=0;
+
+        for (int i=0;i<arr.length;i++){
+            if(arr[i] % 2==0){
+                evenArr [e] = arr[i];
+                e++;
+            }else {
+                oddArr [o] = arr[i];
+                o++;
+            }
+        }
+        System.out.println(Arrays.toString(evenArr));
+        System.out.println(Arrays.toString(oddArr));
+    }
+}
